66b2de47b933c83f6ad2f9bd5864bc3a2acb0e29
Ana Belén <ana.b.95@hotmail.com>
2023-05-28 14:40:51 +0200

    flip cards

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -3,11 +3,19 @@ import "./style.css";
 export const GameCard = ({ game }) => {
   return (
     <article className="gameCard">
-      <img src={game.image} alt={game.title} />
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src={game.image} alt={game.title} />
+          </div>
+          <div className="flip-card-back">
+            <p>{game.text}</p>
+          </div>
+        </div>
+      </div>
       <h2>
         {game.title} ({game.date}) for {game.platform}
       </h2>
-      <p>{game.text}</p>
     </article>
   );
 };
